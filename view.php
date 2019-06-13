aa507292398f308562ab08c363288a7f101f9750
BuriedBullet <49572567+BuriedBullet@users.noreply.github.com>
2019-06-13 09:39:18 -0300

    Update view.php

--- view.php
+++ view.php
@@ -6,3 +6,4 @@ include 'navbar/nav_view.php' ?>
 
 
 <?php include 'footer.php' ?>
+// Fazer a divisão de categorias de cada categoria, mostrar apenas algumas,para ver todas, colocar um collapse para mostatrto que estava embaixo
